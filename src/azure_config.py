1553d8d1c574c9ce5dceb3155e893c9f88cfd678
Giaus <74120584+Buddha0338@users.noreply.github.com>
2025-02-02 15:11:05 -0500

    Delete src directory

--- src/azure_config.py
+++
@@ -1,2 +0,0 @@
-AZURE_ENDPOINT = "https://concordia-animate.cognitiveservices.azure.com/"
-AZURE_KEY = "946bc465a52f4f278820e223f2ace8e3"
--- src/helpers.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-from tensorflow.keras.preprocessing import image
-from tensorflow.keras.applications.mobilenet_v2 import preprocess_input
-
-def preprocess_image(img_path):
-    img = image.load_img(img_path, target_size=(224, 224))
-    img_array = image.img_to_array(img)
-    img_array = np.expand_dims(img_array, axis=0)
-    return preprocess_input(img_array)
